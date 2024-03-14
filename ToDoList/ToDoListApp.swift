667bc589bb5387c5c41bcaae2a0c9243ff886d7b
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-03-14 23:46:23 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by ชลิศา ธรรมราช on 14/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
